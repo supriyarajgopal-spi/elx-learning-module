3f7f9ae83a3fd7376adfc96930cce21ac75211bc
Ryan O'Reilly <ryan.oreilly@thenewgroup.com>
2014-12-15 13:06:44 -0800

    Adding custom utility module

---
+++ sites/all/modules/custom/elx_utility/admin.js
@@ -0,0 +1,8 @@
+(function ($, Drupal, window, document, undefined) {
+Drupal.behaviors.admin_js = {
+  attach: function(context, settings) {
+    $('.node-type-hot-spots .field-name-field-hot-spot-image .image-preview').prepend('<div id="overlay"></div>');
+    console.log('Works');
+  }
+}
+})(jQuery, Drupal, this, this.document);

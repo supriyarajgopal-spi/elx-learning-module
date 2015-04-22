32abcc5554a185ade8691e50a8ae69614e913051
Ryan O'Reilly <ryan.oreilly@thenewgroup.com>
2015-04-21 17:28:29 -0700

    adding JS library to prohibit special characters in title fields

--- sites/all/modules/custom/elx_utility/admin.js
+++ sites/all/modules/custom/elx_utility/admin.js
@@ -90,6 +90,12 @@
         '#tools-node-form div[id^="edit-field-markets-"],' +
         '#badge-node-form div[id^="edit-field-markets-"],' +
         '#disclaimer-node-form div[id^="edit-field-markets-"]').drupalCheckAll(options);
+
+      // Removes special characters from Administrative title field
+      $('.field-name-title-field input').alphanum({
+        disallow   : '?$#!@&()',
+        allow      : '-.',
+      });
     }
   }
 })(jQuery, Drupal, this, this.document);

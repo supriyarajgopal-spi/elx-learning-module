126055999f5c561d9b233a17ffbf3c9ce8de238a
Peter Choi <peter.choi@spi.com>
2016-09-08 11:21:46 -0600

    Jira 25 - github 10 - tools modals fixed - ajax modal render derivation now chained to particular dom using jquery ready to draw variant modal

--- sites/all/themes/elx_front/js/script.js
+++ sites/all/themes/elx_front/js/script.js
@@ -26,183 +26,189 @@
 
         // click should trigger function with overlay
         var linkhref = $(this).attr('href'); /* /modal/nojs/9309 */
-        // var linktitle = $(this).text();
-        // var itemtitle = $('<div class="item-title-inmodal">' + linktitle + '</div>');
 
         // future perfect tense-ification //
-        setTimeout(function () {
+        var launchModal = function () {
 
-          // grab title field in all content variations //
-          var display_title = $('#modalContent .field-name-field-display-title').find('.field-item.even').html();
-          var itemtitle = $('<div class="item-title-inmodal">' + display_title + '</div>');
+          setTimeout(function () {
 
-          var close_action = function () {
+            // uat debug // remove after
+            var typefile = $('#modal-content .field-name-field-tool-pdf.field-type-file').html();
+            console.log(typefile);
 
-            // flush out prior content // required because modal content is 3 variations per 1 agnostic payload that can be any of 3 types //
-            $('body.page-tools.section-tools #modalContent').hide();
-            $('body.page-tools.section-tools #modalContent .node-tools').empty();
-            $('body.page-tools.section-tools #modalBackdrop').hide().empty();
+            // grab title field in all content variations //
+            var display_title = $('#modalContent .field-name-field-display-title').find('.field-item.even').html();
+            var itemtitle = $('<div class="item-title-inmodal">' + display_title + '</div>');
+
+            var close_action = function () {
+
+              // flush out prior content // required because modal content is 3 variations per 1 agnostic payload that can be any of 3 types //
+              $('body.page-tools.section-tools #modalContent').hide();
+              $('body.page-tools.section-tools #modalContent .node-tools').empty();
+              $('body.page-tools.section-tools #modalBackdrop').hide().empty();
+              $('body.page-tools.section-tools #modalContent .node-tools').remove();
+
+              $('body.page-tools.section-tools').removeClass('ctools-modal-open').addClass('ctools-modal-close');
+
+            };
+
+            var isVideo = function () {
+              return ($('body.page-tools.section-tools #modalContent .node-tools').find('.content video source').attr('src'));
+            };
+            var isPDF = function () {
+              return ($('body.page-tools.section-tools #modalContent .node-tools').find('.content a').attr('href'));
+            };
+            var isAnimGif = function () {
+              return ($('body.page-tools.section-tools #modalContent .node-tools').find('.content img').attr('src'));
+            };
+
+            var out;
+            var getVideoContent = function () {
+
+              var vout = $('body.page-tools.section-tools #modalContent .node-tools .content');
+              out = vout.html();
+
+            };
+            var getAnimGifContent = function () {
+
+              var pout = $('body.page-tools.section-tools #modalContent .node-tools .field-name-field-tool-description .field-item.even p').addClass('animgif-paragraph');
+              out = $('body.page-tools.section-tools #modalContent .node-tools .field-name-field-tool-description .field-item.even p').html();
+              out = pout.html();
+
+            };
+
+
+            var asset;
+
+            // establish main asset variable for 3 version variation for modal views at FE //
+            if (isVideo()) { asset = isVideo(); getVideoContent(); }
+            else if (isPDF()) { asset = isPDF(); }
+            else if (isAnimGif()) { asset = isAnimGif(); getAnimGifContent(); }
+
+
+            // works fine but without body tag //
+            // var asset = $('body.page-tools.section-tools #modalContent').find('.node-tools .field-name-field-tool-pdf').find('.field-items .field-item').html();
+            var innerpart_newcontainer = $('body.page-tools.section-tools #modalContent .node-tools');
+            var modalheader = $('<div class="modal-header"><a class="close"></a></div>').click(close_action);
+            // console.log(innerpart); // var outim = innerpart.html(); alert(outim);
+
+            $('body.page-tools.section-tools #modalBackdrop').css({'background-color': '#040A2B', 'opacity': '0.8'});
+            $('body.page-tools.section-tools #modalContent').css({'border': '1px solid black', 'width': '50%', 'margin-top': '50px', 'margin-left': '10px', 'background-color': 'white', 'overflow': 'visible'});
+
+            var btnlink = $('<a href="' + asset + '" data-node-id="' + linkhref + '" target="_blank">VIEW TOOL</a>');
+            var viewbtn = $('<div id="detail-content-btn"></div>');
+            $(viewbtn).append(btnlink);
+
+            // rm dupes and prior loaded content // because it can be any of 3 types //
             $('body.page-tools.section-tools #modalContent .node-tools').remove();
 
-            $('body.page-tools.section-tools').removeClass('ctools-modal-open').addClass('ctools-modal-close');
+            $('body.page-tools.section-tools #modalContent').find('.modal-header').remove();
+            $('body.page-tools.section-tools #modalContent').find('#detail-content-btn').remove();
+            $('body.page-tools.section-tools #modalContent video').remove();
+            $('body.page-tools.section-tools #modalContent img.animated-gif').remove();
+            $('body.page-tools.section-tools #modalContent .paragraph-animgif-container').remove();
+            $('body.page-tools.section-tools #modalContent .item-title-inmodal').remove();
 
-          };
+            // add new container after removing prior content in modal accumilated //
+            $('body.page-tools.section-tools #modalContent').prepend(modalheader).append(innerpart_newcontainer);
 
-          var isVideo = function () {
-            return ($('body.page-tools.section-tools #modalContent .node-tools').find('.content video source').attr('src'));
-          };
-          var isPDF = function () {
-            return ($('body.page-tools.section-tools #modalContent .node-tools').find('.content a').attr('href'));
-          };
-          var isAnimGif = function () {
-            return ($('body.page-tools.section-tools #modalContent .node-tools').find('.content img').attr('src'));
-          };
+            if (asset.match(/pdf/i)) {
 
-          var out;
-          var getVideoContent = function () {
+              $('body.page-tools.section-tools #modalContent').append(itemtitle).append(viewbtn);
 
-            var vout = $('body.page-tools.section-tools #modalContent .node-tools .content');
-            out = vout.html();
+            }
+            else if (asset.match(/mp4/i)) {
 
-          };
-          var getAnimGifContent = function () {
+              $('body.page-tools.section-tools #modalContent').append(itemtitle).append(out);
 
-            var pout = $('body.page-tools.section-tools #modalContent .node-tools .field-name-field-tool-description .field-item.even p').addClass('animgif-paragraph');
-            out = $('body.page-tools.section-tools #modalContent .node-tools .field-name-field-tool-description .field-item.even p').html();
-            out = pout.html();
+            }
+            else if (asset.match(/gif/i)) {
 
-          };
+              $('body.page-tools.section-tools #modalContent').css({'width': '50%', 'margin-left': '30px'});
 
+              var animgif = $('<img src="' + asset + '" class="animated-gif" />');
+              var panim = $('<div class="paragraph-animgif-container"></div>');
+              $('body.page-tools.section-tools #modalContent').append(itemtitle);
+              $(panim).append(out).append(animgif);
+              $('body.page-tools.section-tools #modalContent').append(panim);
 
-          var asset;
-
-          // establish main asset variable for 3 version variation for modal views at FE //
-          if (isVideo()) { asset = isVideo(); getVideoContent(); }
-          else if (isPDF()) { asset = isPDF(); }
-          else if (isAnimGif()) { asset = isAnimGif(); getAnimGifContent(); }
+            }
 
 
-          // works fine but without body tag //
-          // var asset = $('body.page-tools.section-tools #modalContent').find('.node-tools .field-name-field-tool-pdf').find('.field-items .field-item').html();
-          var innerpart_newcontainer = $('body.page-tools.section-tools #modalContent .node-tools');
-          var modalheader = $('<div class="modal-header"><a class="close"></a></div>').click(close_action);
-          // console.log(innerpart); // var outim = innerpart.html(); alert(outim);
+            $('body.page-tools.section-tools #modalContent').find('.item-title-inmodal').css('display', 'block');
 
-          $('body.page-tools.section-tools #modalBackdrop').css({'background-color': '#040A2B', 'opacity': '0.8'});
-          $('body.page-tools.section-tools #modalContent').css({'border': '1px solid black', 'width': '50%', 'margin-top': '50px', 'margin-left': '10px', 'background-color': 'white', 'overflow': 'visible'});
+            // rm after consuming data points //
+            $('body.page-tools.section-tools #modalContent .node-tools').css('display', 'none');
 
-          var btnlink = $('<a href="' + asset + '" data-node-id="' + linkhref + '" target="_blank">VIEW TOOL</a>');
-          var viewbtn = $('<div id="detail-content-btn"></div>');
-          $(viewbtn).append(btnlink);
+            // opacity 1 only after new add - item-title-inmodal
+            $('body.page-tools.section-tools #modalContent .item-title-inmodal').css('opacity', '1');
 
-          // rm dupes and prior loaded content // because it can be any of 3 types //
-          $('body.page-tools.section-tools #modalContent .node-tools').remove();
+            // tail end clean up of consumed DOM node breakout to 3 variants //
+            $('.ctools-modal-content #modal-content').css({
+              display: 'none'
+            });
+            $('.ctools-modal-content').css({
+              display: 'none'
+            });
 
-          $('body.page-tools.section-tools #modalContent').find('.modal-header').remove();
-          $('body.page-tools.section-tools #modalContent').find('#detail-content-btn').remove();
-          $('body.page-tools.section-tools #modalContent video').remove();
-          $('body.page-tools.section-tools #modalContent img.animated-gif').remove();
-          $('body.page-tools.section-tools #modalContent .paragraph-animgif-container').remove();
-          $('body.page-tools.section-tools #modalContent .item-title-inmodal').remove();
+            // center using existing facility // resize not just initial parts that render unresize at first
+            $(window).trigger('resize');
 
-          // add new container after removing prior content in modal accumilated //
-          $('body.page-tools.section-tools #modalContent').prepend(modalheader).append(innerpart_newcontainer);
-
-          if (asset.match(/pdf/i)) {
-
-            $('body.page-tools.section-tools #modalContent').append(itemtitle).append(viewbtn);
-
-          }
-          else if (asset.match(/mp4/i)) {
-
-            $('body.page-tools.section-tools #modalContent').append(itemtitle).append(out);
-
-          }
-          else if (asset.match(/gif/i)) {
-
-            $('body.page-tools.section-tools #modalContent').css({'width': '50%', 'margin-left': '30px'});
-
-            var animgif = $('<img src="' + asset + '" class="animated-gif" />');
-            var panim = $('<div class="paragraph-animgif-container"></div>');
-            $('body.page-tools.section-tools #modalContent').append(itemtitle);
-            $(panim).append(out).append(animgif);
-            $('body.page-tools.section-tools #modalContent').append(panim);
-
-          }
-
-
-          $('body.page-tools.section-tools #modalContent').find('.item-title-inmodal').css('display', 'block');
-
-          // rm after consuming data points //
-          $('body.page-tools.section-tools #modalContent .node-tools').css('display', 'none');
-
-          // opacity 1 only after new add - item-title-inmodal
-          $('body.page-tools.section-tools #modalContent .item-title-inmodal').css('opacity', '1');
-
-          // tail end clean up of consumed DOM node breakout to 3 variants //
-          $('.ctools-modal-content #modal-content').css({
-            display: 'none'
-          });
-          $('.ctools-modal-content').css({
-            display: 'none'
-          });
-
-          // center using existing facility // resize not just initial parts that render unresize at first
-          $(window).trigger('resize');
-
-        }, 500);
-
-      });
-
-      /* SEARCH #edit-combine search field alteration */
-      if ($('body.page-search-product-library.section-search-product-library').size()) {
-
-        // build markup for
-        var search_container = $('<div id="search-line-rearranged" />');
-        var search_icon_component = $('<div class="icon-component"><div class="icon-holder-placement"><div class="icon-holder" /></div></div>');
-        var search_field = $('<input type="text" id="search-line-pipe" />').attr('maxlength', 128).attr('size', 30).attr('name', 'search-line');
-
-        $(search_container).append(search_field).append(search_icon_component);
-
-        // component form action //
-        var submitSearchForm = function () {
-
-          var search_line_value = $('main #search-line-pipe').val();
-          $('form#views-exposed-form-search-product-library-page').find('input#edit-combine').attr('value', search_line_value);
-
-          return $('form#views-exposed-form-search-product-library-page').find('input#edit-submit-search-product-library').trigger('click');
+          }, 500);
 
         };
-
-        // pass enter events through to form action //
-        $(search_field).on('keypress', function (keyevent) {
-          if (keyevent.keyCode === 13) {
-            submitSearchForm();
-          }
-        });
-        $(search_container).find('.icon-holder').click(submitSearchForm);
-
-        // set only 1 component //
-        if (!$('#search-line-rearranged').size()) { $(search_container).insertBefore('main .view.view-search-product-library'); }
-
-      }
-
-      /* LEVELS */
-      if ($('body.page-levels-all').length || $('body.page-levels-complete').length || $('body.page-levels-in-progress').length) {
-
-        /* eslint-disable no-alert, no-console */
-        $('#modalContent').ready(function () {
-          var mClass = '';
-          mClass = $('#modalContent .field-name-field-learning-category .field-item').text();
-          mClass = mClass.replace(/\s+/g, '-').toLowerCase();
-          console.log('class = ' + mClass);
-          $('#modalContent').addClass(mClass);
-        });
-
-        /* eslint-enable no-alert, no-console */
+        // });
+        $('#modal-content .field-name-field-tool-pdf.field-type-file').ready(launchModal); // function() {
 
 
-      }
+        /* SEARCH #edit-combine search field alteration */
+        if ($('body.page-search-product-library.section-search-product-library').size()) {
+
+          // build markup for
+          var search_container = $('<div id="search-line-rearranged" />');
+          var search_icon_component = $('<div class="icon-component"><div class="icon-holder-placement"><div class="icon-holder" /></div></div>');
+          var search_field = $('<input type="text" id="search-line-pipe" />').attr('maxlength', 128).attr('size', 30).attr('name', 'search-line');
+
+          $(search_container).append(search_field).append(search_icon_component);
+
+          // component form action //
+          var submitSearchForm = function () {
+
+            var search_line_value = $('main #search-line-pipe').val();
+            $('form#views-exposed-form-search-product-library-page').find('input#edit-combine').attr('value', search_line_value);
+
+            return $('form#views-exposed-form-search-product-library-page').find('input#edit-submit-search-product-library').trigger('click');
+
+          };
+
+          // pass enter events through to form action //
+          $(search_field).on('keypress', function (keyevent) {
+            if (keyevent.keyCode === 13) {
+              submitSearchForm();
+            }
+          });
+          $(search_container).find('.icon-holder').click(submitSearchForm);
+
+          // set only 1 component //
+          if (!$('#search-line-rearranged').size()) { $(search_container).insertBefore('main .view.view-search-product-library'); }
+
+        }
+
+        /* LEVELS */
+        if ($('body.page-levels-all').length || $('body.page-levels-complete').length || $('body.page-levels-in-progress').length) {
+
+          /* eslint-disable no-alert, no-console */
+          $('#modalContent').ready(function () {
+            var mClass = '';
+            mClass = $('#modalContent .field-name-field-learning-category .field-item').text();
+            mClass = mClass.replace(/\s+/g, '-').toLowerCase();
+            console.log('class = ' + mClass);
+            $('#modalContent').addClass(mClass);
+          });
+
+        }
+
+      }); // ready
 
     }
   };

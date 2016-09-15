055bc5bbe8d52efd10b47d596d47b72c86131902
Darren Oh <darren.oh@spi.com>
2016-09-15 11:15:24 -0400

    Issue #68: Fixed guest URL redirect.

--- sites/all/modules/custom/elx_guest_url/js/guest_url_redirect.js
+++ sites/all/modules/custom/elx_guest_url/js/guest_url_redirect.js
@@ -3,16 +3,13 @@
  * Borrowed from https://css-tricks.com/snippets/javascript/get-url-and-url-parts-in-javascript/
  */
 (function ($) {
-	Drupal.behaviors.elx_guest_url = {
-	  attach: function (context, settings) {
-		var pathArray = window.location.href.split(window.location.pathname); //Split the full URL based on pathname
-		var queryString = pathArray[1].split('?'); //Get the query string
-		var newURL = window.location.protocol + "//" + window.location.host + window.location.pathname + "signup"; //Form the new URL
-		
-		if(queryString[1] !== undefined) //If query string exists, append to the new URL
-			newURL = newURL + '?' + queryString[1];
-	
-		window.location.replace(newURL); //Redirect to new URL
-	  }
-	};
+  Drupal.behaviors.elx_guest_url = {
+    attach: function (context, settings) {
+      var pathArray = location.hash.split('?');
+      if (pathArray[0] == 'signup') {
+        // Redirect to new URL
+        window.location.replace(Drupal.settings.basePath + 'user/register?' + pathArray[1]);
+      }
+    }
+  };
 }(jQuery));

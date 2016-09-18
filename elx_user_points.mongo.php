1fb9676db68aa9de93e7c1ad92a2b06f357cbfe7
Darren Oh <darren.oh@spi.com>
2016-09-18 19:41:49 -0400

    Issue #40: Added error recovery to user points migration.

--- elx_user_points.mongo.php
+++ elx_user_points.mongo.php
@@ -1,161 +1,209 @@
 <?php
+
 /**
  * Root directory of Drupal installation.
  */
 define('DRUPAL_ROOT', getcwd());
 
-include_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 drupal_set_time_limit(240);
-require '/usr/local/Cellar/composer/1.1.2/libexec/vendor/autoload.php';
-
-$col_count = 0;
 $userpoint_vocabulary = taxonomy_vocabulary_machine_name_load('userpoints');
-$vid = $userpoint_vocabulary->vid;
-
-//$mongodb = 'mongodb://stagemyelx.cloudapp.net:27017/mean-prod';
-$mongodb = 'mongodb://myelx.cloudapp.net:27017/mean-prod';
-$options = ['connectTimeoutMS' => 60000, 'socketTimeoutMS' => 60000];
-$mongo = new MongoDB\Client($mongodb, $options);
-$collection = $mongo->selectCollection('mean-prod', 'userPoints');
-//$collection_node = $mongo->selectCollection('mean-prod', 'node');
-$cursor = $collection->find();
-
-
-foreach ($cursor as $obj) {
-  $col_count = $col_count + 1;
-  $user_uid = $obj->uid;
-  $points = $obj->points;
-  $point_type = $obj->kind;
-  $user_points[] = array($user_uid, $points, $point_type);
-/*  
-  // Create query for second collection
-  $qry = array('_id' => $mongo_id);
-  $cursor_node = $collection_node->find($qry);
-  foreach ($cursor_node as $obj_node) {
-  	$node_id = $obj_node->nid;
-  	  print_r($nid);
+require '/usr/local/Cellar/composer/1.1.2/libexec/vendor/autoload.php';
+$mongo = new MongoDB\Client('mongodb://myelx.cloudapp.net:27017/mean-prod', array(
+  'connectTimeoutMS' => 60000,
+  'socketTimeoutMS' => 60000,
+));
+$database = $mongo->{'mean-prod'};
+// Ensure that processing resumes if the connection to Mongo is lost.
+do {
+  try {
+    $cursor = elx_user_points_cursor($database);
+    // Store object ID of the last successfully processed record.
+    $saved_id = NULL;
+    while (!empty($cursor)) {
+      elx_user_points_batch($cursor, $database, $userpoint_vocabulary->vid, $saved_id);
+      $cursor = elx_user_points_cursor($database, $saved_id);
+    }
+    $disconnected = FALSE;
   }
-*/  
-  if ($points != 0) {
-  
-    // Set product userpoint term
-    if ($point_type == 'product') {
-      $mongo_term_name = 'product';
+  catch (MongoDB\Driver\Exception\ConnectionException $e) {
+    $disconnected = TRUE;
+  }
+}
+while ($disconnected);
+
+/**
+ * Return the result of a user points query for batch processing.
+ *
+ * @param MongoDB\Database $database
+ *   Mongo database to query.
+ * @param MongoDB\BSON\ObjectID $saved_id
+ *   Object ID of the last successfully processed record.
+ *
+ * @return MongoDB\Driver\Cursor
+ *   User points query result for batch processing.
+ */
+function elx_user_points_cursor(MongoDB\Database $database, MongoDB\BSON\ObjectID $saved_id = NULL) {
+  if (isset($saved_id)) {
+    $query = array(
+      '_id' => array('$gt' => $saved_id),
+    );
+  }
+  else {
+    $query = array();
+  }
+  return $database->userPoints->find($query, array(
+    'sort' => array('_id' => 1),
+    'limit' => 1,
+  ));
+}
+
+/**
+ * Processes a batch of user points.
+ *
+ * @param MongoDB\Driver\Cursor $cursor
+ *   User points query result to process.
+ * @param MongoDB\Database $database
+ *   Mongo database for additional queries.
+ * @param int $vid
+ *   User points vocabulary ID.
+ */
+function elx_user_points_batch(MongoDB\Driver\Cursor $cursor, MongoDB\Database $database, $vid, &$saved_id) {
+  foreach ($cursor as $obj) {
+    $user_uid = $obj->uid;
+    $points = $obj->points;
+    $point_type = $obj->kind;
+    $user_points[] = array($user_uid, $points, $point_type);
+  /*  
+    // Create query for second collection
+    $qry = array('_id' => $mongo_id);
+    $cursor_node = $collection_node->find($qry);
+    foreach ($cursor_node as $obj_node) {
+      $node_id = $obj_node->nid;
+        print_r($nid);
+    }
+  */  
+    if ($points != 0) {
+
+      // Set product userpoint term
+      if ($point_type == 'product') {
+        $mongo_term_name = 'product';
+        $tid = get_userpoint_term_tid($mongo_term_name, $vid);
+        if ($tid == FALSE) {
+       $tid = set_userpoint_taxonomy('product', $vid);
+      }
+      }
+
+      if (isset($obj->group)) {
+        $group_type = $obj->group;
+      $mongo_term_name = $group_type . '_' . $point_type;
       $tid = get_userpoint_term_tid($mongo_term_name, $vid);
-      if ($tid == FALSE) {
-	   $tid = set_userpoint_taxonomy('product', $vid);
-	  }
-    }
+        if ($tid == FALSE) {
+          $tid = set_userpoint_taxonomy($mongo_term_name, $vid);
+        }
+      }
+      else {
+        $group_type = '';
+      }
 
-    if (isset($obj->group)) {
-      $group_type = $obj->group;
-	  $mongo_term_name = $group_type . '_' . $point_type;
-	  $tid = get_userpoint_term_tid($mongo_term_name, $vid);
-	    if ($tid == FALSE) {
-	  	  $tid = set_userpoint_taxonomy($mongo_term_name, $vid);
-	    }
-    }
-    else {
-  	  $group_type = '';
-    }
+      // Set first-login userpoint term
+      if (isset($obj->contentid)) {
+        $contentid = $obj->contentid;
+      if ($contentid == 'first-login') {
+        $mongo_term_name = 'first-login';
+        $tid = get_userpoint_term_tid($mongo_term_name, $vid);
+        if ($tid == FALSE) {
+          $tid = set_userpoint_taxonomy('first-login', $vid);
+        }
+      }
+      }
 
-    // Set first-login userpoint term
-    if (isset($obj->contentid)) {
-  	  $contentid = $obj->contentid;
-	  if ($contentid == 'first-login') {
-	    $mongo_term_name = 'first-login';
-	    $tid = get_userpoint_term_tid($mongo_term_name, $vid);
-	    if ($tid == FALSE) {
-	  	  $tid = set_userpoint_taxonomy('first-login', $vid);
-	    }
-	  }
-    }
+      // Insert user points into userpoints, userpoints_total, and userpoints_txn tables
 
-    // Insert user points into userpoints, userpoints_total, and userpoints_txn tables
-    
-    // Check userpoints table for already created user/tid combo
-    $userpoints_points = get_user_points($user_uid, $tid);
-  
-    if ($userpoints_points == FALSE) { 
-      $pid = db_insert('userpoints')
+      // Check userpoints table for already created user/tid combo
+      $userpoints_points = get_user_points($user_uid, $tid);
+
+      if ($userpoints_points == FALSE) { 
+        $pid = db_insert('userpoints')
+          ->fields(array(
+            'uid'         => $user_uid,
+            'points'      => $points,
+            'max_points'  => $points,
+            'last_update' => REQUEST_TIME,
+            'tid'         => $tid,
+        ))
+        ->execute();
+        if (empty($pid) || $pid == FALSE) {
+          $error[$user_uid]['error'] = $pid . ':' . $user_uid;
+        }
+      }
+      else {
+        $points = $userpoints_points + $points;
+        $fid = db_update('userpoints')
+        ->fields(array(
+          'points'       => $points,
+          'max_points'   => $points,
+          'last_update'  => REQUEST_TIME,
+          ))
+      ->condition('uid', $user_uid, '=')
+        ->execute(); 
+      }
+
+      // Check userpoints_total table for points
+      $user_total_points = get_points_for_userpoints_total($user_uid);
+
+      if ($user_total_points != FALSE) {
+        $total_points = $user_total_points + $points;
+        $fid = db_update('userpoints_total')
+        ->fields(array(
+          'points'       => $total_points,
+          'max_points'   => $total_points,
+          'last_update'  => REQUEST_TIME,
+          ))
+      ->condition('uid', $user_uid, '=')
+        ->execute();
+      }
+      else {
+        $user_point_total_id = db_insert('userpoints_total')
         ->fields(array(
           'uid'         => $user_uid,
           'points'      => $points,
           'max_points'  => $points,
           'last_update' => REQUEST_TIME,
-          'tid'         => $tid,
+        ))
+        ->execute();
+        if (empty($user_point_total_id) || $user_point_total_id == FALSE) {
+          $error_total[$user_uid]['error'] = $points . ':' . $user_uid;
+        }
+      }
+
+       $txn_id = db_insert('userpoints_txn')
+        ->fields(array(
+          'uid'           => $user_uid,
+          'approver_uid'  => 0,
+          'points'        => $points,
+          'time_stamp'    => REQUEST_TIME,
+          'changed'       => REQUEST_TIME,
+          'status'        => 0,
+          'description'   => $mongo_term_name, 
+          'reference'     => $user_uid,
+          'expirydate'    => 0,
+          'expired'       => 0,
+          'parent_txn_id' => 0,
+          'tid'           => $tid,
+          'entity_id'     => $user_uid,
+          'entity_type'   => 'user',
+          'operation'     => 'Insert',
       ))
       ->execute();
       if (empty($pid) || $pid == FALSE) {
         $error[$user_uid]['error'] = $pid . ':' . $user_uid;
-      }
+      } 
     }
-    else {
-      $points = $userpoints_points + $points;
-      $fid = db_update('userpoints')
-      ->fields(array(
-        'points'       => $points,
-        'max_points'   => $points,
-        'last_update'  => REQUEST_TIME,
-        ))
-	  ->condition('uid', $user_uid, '=')
-      ->execute(); 
-    }
- 
-    // Check userpoints_total table for points
-    $user_total_points = get_points_for_userpoints_total($user_uid);
-   
-    if ($user_total_points != FALSE) {
-      $total_points = $user_total_points + $points;
-      $fid = db_update('userpoints_total')
-      ->fields(array(
-        'points'       => $total_points,
-        'max_points'   => $total_points,
-        'last_update'  => REQUEST_TIME,
-        ))
-	  ->condition('uid', $user_uid, '=')
-      ->execute();
-    }
-    else {
-      $user_point_total_id = db_insert('userpoints_total')
-      ->fields(array(
-        'uid'         => $user_uid,
-        'points'      => $points,
-        'max_points'  => $points,
-        'last_update' => REQUEST_TIME,
-      ))
-      ->execute();
-      if (empty($user_point_total_id) || $user_point_total_id == FALSE) {
-        $error_total[$user_uid]['error'] = $points . ':' . $user_uid;
-      }
-    }
-    
-     $txn_id = db_insert('userpoints_txn')
-      ->fields(array(
-        'uid'           => $user_uid,
-        'approver_uid'  => 0,
-        'points'        => $points,
-        'time_stamp'    => REQUEST_TIME,
-        'changed'       => REQUEST_TIME,
-        'status'        => 0,
-        'description'   => $mongo_term_name, 
-        'reference'     => $user_uid,
-        'expirydate'    => 0,
-        'expired'       => 0,
-        'parent_txn_id' => 0,
-        'tid'           => $tid,
-        'entity_id'     => $user_uid,
-        'entity_type'   => 'user',
-        'operation'     => 'Insert',
-    ))
-    ->execute();
-    if (empty($pid) || $pid == FALSE) {
-      $error[$user_uid]['error'] = $pid . ':' . $user_uid;
-    } 
+    $saved_id = $obj->_id;
   }
 }
-
 
 function get_points_for_userpoints_total($uid) {
   $result = db_select('userpoints_total', 'upt')

275fc306c97d469bb0114da11e93d21957900ee0
Tracy Myers <tracy.myers@spi.com>
2016-09-19 23:23:23 -0600

    Issue #40 - Added code to collect node_id if found from mongo node collection

--- elx_user_points.mongo.php
+++ elx_user_points.mongo.php
@@ -55,7 +55,7 @@ function elx_user_points_cursor(MongoDB\Database $database, MongoDB\BSON\ObjectI
   }
   return $database->userPoints->find($query, array(
     'sort' => array('_id' => 1),
-    'limit' => 1,
+    'limit' => 10,
   ));
 }
 
@@ -71,19 +71,12 @@ function elx_user_points_cursor(MongoDB\Database $database, MongoDB\BSON\ObjectI
  */
 function elx_user_points_batch(MongoDB\Driver\Cursor $cursor, MongoDB\Database $database, $vid, &$saved_id) {
   foreach ($cursor as $obj) {
+  	$mongo_term_name = '';
     $user_uid = $obj->uid;
     $points = $obj->points;
     $point_type = $obj->kind;
     $user_points[] = array($user_uid, $points, $point_type);
-  /*  
-    // Create query for second collection
-    $qry = array('_id' => $mongo_id);
-    $cursor_node = $collection_node->find($qry);
-    foreach ($cursor_node as $obj_node) {
-      $node_id = $obj_node->nid;
-        print_r($nid);
-    }
-  */  
+    
     if ($points != 0) {
 
       // Set product userpoint term
@@ -91,14 +84,14 @@ function elx_user_points_batch(MongoDB\Driver\Cursor $cursor, MongoDB\Database $
         $mongo_term_name = 'product';
         $tid = get_userpoint_term_tid($mongo_term_name, $vid);
         if ($tid == FALSE) {
-       $tid = set_userpoint_taxonomy('product', $vid);
-      }
+          $tid = set_userpoint_taxonomy('product', $vid);
+        }
       }
 
       if (isset($obj->group)) {
         $group_type = $obj->group;
-      $mongo_term_name = $group_type . '_' . $point_type;
-      $tid = get_userpoint_term_tid($mongo_term_name, $vid);
+        $mongo_term_name = $group_type . '_' . $point_type;
+        $tid = get_userpoint_term_tid($mongo_term_name, $vid);
         if ($tid == FALSE) {
           $tid = set_userpoint_taxonomy($mongo_term_name, $vid);
         }
@@ -110,14 +103,33 @@ function elx_user_points_batch(MongoDB\Driver\Cursor $cursor, MongoDB\Database $
       // Set first-login userpoint term
       if (isset($obj->contentid)) {
         $contentid = $obj->contentid;
-      if ($contentid == 'first-login') {
-        $mongo_term_name = 'first-login';
-        $tid = get_userpoint_term_tid($mongo_term_name, $vid);
-        if ($tid == FALSE) {
-          $tid = set_userpoint_taxonomy('first-login', $vid);
+        if ($contentid == 'first-login') {
+          $mongo_term_name = 'first-login';
+          $tid = get_userpoint_term_tid($mongo_term_name, $vid);
+          if ($tid == FALSE) {
+            $tid = set_userpoint_taxonomy('first-login', $vid);
+          }
         }
       }
-      }
+	  
+	  if ($mongo_term_name != 'first-login') {
+	  	$entity_type = 'node';
+	    // Create and query node collection to retrive nid
+        $qry = array('_id' => $saved_id);
+        $cursor_node = $database->node->find($qry);
+        foreach ($cursor_node as $obj_node) {
+          $node_title = $obj->title;
+          $node_id = $obj_node->nid;
+		  $nid = get_nid_for_points($node_id);
+		  if ($nid != FALSE) {
+            $reference = $nid;
+          }
+        }
+	  }
+	  else {
+	  	$entity_type = 'user';
+		$reference = $user_uid;
+	  }
 
       // Insert user points into userpoints, userpoints_total, and userpoints_txn tables
 
@@ -146,7 +158,7 @@ function elx_user_points_batch(MongoDB\Driver\Cursor $cursor, MongoDB\Database $
           'max_points'   => $points,
           'last_update'  => REQUEST_TIME,
           ))
-      ->condition('uid', $user_uid, '=')
+        ->condition('uid', $user_uid, '=')
         ->execute(); 
       }
 
@@ -161,7 +173,7 @@ function elx_user_points_batch(MongoDB\Driver\Cursor $cursor, MongoDB\Database $
           'max_points'   => $total_points,
           'last_update'  => REQUEST_TIME,
           ))
-      ->condition('uid', $user_uid, '=')
+        ->condition('uid', $user_uid, '=')
         ->execute();
       }
       else {
@@ -178,7 +190,7 @@ function elx_user_points_batch(MongoDB\Driver\Cursor $cursor, MongoDB\Database $
         }
       }
 
-       $txn_id = db_insert('userpoints_txn')
+      $txn_id = db_insert('userpoints_txn')
         ->fields(array(
           'uid'           => $user_uid,
           'approver_uid'  => 0,
@@ -187,24 +199,31 @@ function elx_user_points_batch(MongoDB\Driver\Cursor $cursor, MongoDB\Database $
           'changed'       => REQUEST_TIME,
           'status'        => 0,
           'description'   => $mongo_term_name, 
-          'reference'     => $user_uid,
+          'reference'     => $reference,
           'expirydate'    => 0,
           'expired'       => 0,
           'parent_txn_id' => 0,
           'tid'           => $tid,
           'entity_id'     => $user_uid,
-          'entity_type'   => 'user',
+          'entity_type'   => $entity_type,
           'operation'     => 'Insert',
       ))
       ->execute();
-      if (empty($pid) || $pid == FALSE) {
-        $error[$user_uid]['error'] = $pid . ':' . $user_uid;
-      } 
+      if (empty($txn_id) || $txn_id == FALSE) {
+        $error[$user_uid]['error'] = $txn_id . ':' . $user_uid;
+      }
+ 
     }
     $saved_id = $obj->_id;
   }
 }
 
+/**
+ * @param uid 
+ *   user's uid
+ * @return
+ *   returns total points for a given user
+ */
 function get_points_for_userpoints_total($uid) {
   $result = db_select('userpoints_total', 'upt')
     ->fields('upt', array('points'))
@@ -220,6 +239,14 @@ function get_points_for_userpoints_total($uid) {
 	}
 }
 
+/** 
+ * @param mongo_tx_name 
+ *   taxonomy name
+ * @param vid 
+ *   taxonomy vocabulary id
+ * @return
+ *   taxonomy term id
+ */
 function get_userpoint_term_tid($mongo_tx_name, $vid) {
   $result = db_select('taxonomy_term_data', 'ttd')
   ->fields('ttd', array('tid'))
@@ -236,6 +263,16 @@ function get_userpoint_term_tid($mongo_tx_name, $vid) {
   }	
 }
 
+/** 
+ * Set's the taxonomy for userpoints and returns the tid 
+ *
+ * @param mongo_tx_name 
+ *   taxonomy name
+ * @param vid 
+ *   taxonomy vocabulary id
+ * @return
+ *   taxonomy term id
+ */
 function set_userpoint_taxonomy($mongo_tx_name, $vid) {
   $term = new stdClass();
   $term->vid = $vid;
@@ -245,7 +282,16 @@ function set_userpoint_taxonomy($mongo_tx_name, $vid) {
   return $tid;
 }
 
-// Returns user points per taxonomy
+/** 
+ * Returns user's points per taxonomy id
+ *
+ * @param user_uid 
+ *   user's id
+ * @param tid 
+ *   taxonomy term id
+ * @return
+ *   userpoints_points
+ */
 function get_user_points($user_uid, $tid) {
   $result = db_select('userpoints', 'up')
   ->fields('up', array('points'))
@@ -259,5 +305,31 @@ function get_user_points($user_uid, $tid) {
   }
   else {
 	return FALSE;
+  }
+}
+
+/** 
+ * Returns user's points per taxonomy id
+ *
+ * @param user_uid 
+ *   user's id
+ * @param tid 
+ *   taxonomy term id
+ * @return
+ *   userpoints_points
+ */
+function get_nid_for_points($nid) {
+  $man_nid = $result[0];
+  $result = db_select('field_revision_field_manifest', 'frfm')
+    ->fields('frfm', array('entity_id'))
+    ->condition('field_manifest_target_id', $nid, '=')
+    ->execute()
+    ->fetchCol();
+  if (!empty($result[0])) {
+    $nid = $result[0];
+    return $nid;
+  }
+  else {
+    return FALSE;
   }
 }
